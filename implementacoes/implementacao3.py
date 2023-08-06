8377ad9d0f9435304ec7edd9b7ab3651cb1a896a
Caio Vinicius <caio.hosana@hotmail.com>
2023-08-06 14:11:35 -0300

    Implementação 4quase completa, não sei se a redistribuicao de cores é assim

--- implementacoes/implementacao3.py
+++ implementacoes/implementacao3.py
@@ -2,9 +2,83 @@ import cv2 as cv
 import sys
 import numpy as np
 
+
+def decompor(img):
+    h, w, channels = img.shape
+    if channels == 2:
+        first = np.zeros_like(img)
+        second = np.zeros_like(img)
+        first[:, :, 0] = img[:, :, 0]
+        second[:, :, 1] = img[:, :, 1]
+        cv.imshow("first", first)
+        cv.imshow("second", second)
+    if channels == 3:
+        first = np.zeros_like(img)
+        second = np.zeros_like(img)
+        third = np.zeros_like(img)
+        first[:, :, 0] = img[:, :, 0]
+        second[:, :, 1] = img[:, :, 1]
+        third[:, :, 2] = img[:, :, 2]
+        cv.imshow("first", first)
+        cv.imshow("second", second)
+        cv.imshow("third", third)
+        cv.waitKey(10000)
+        cv.destroyAllWindows()
+    if channels == 4:
+        # print(img[:, :, 0])
+        first = np.zeros_like(img)
+        second = np.zeros_like(img)
+        third = np.zeros_like(img)
+        fourth = np.zeros_like(img)
+        first[:, :, 0] = img[:, :, 0]
+        second[:, :, 1] = img[:, :, 1]
+        third[:, :, 2] = img[:, :, 2]
+        fourth[:, :, 2] = img[:, :, 2]
+        cv.imshow("first", first)
+        cv.imshow("second", second)
+        cv.imshow("fourth", fourth)
+        cv.imshow("third", third)
+        cv.waitKey(10000)
+        cv.destroyAllWindows()
+
+
+def cvtBGR2CMYK(img):
+    B = img[:, :, 0].astype(float)
+    G = img[:, :, 1].astype(float)
+    R = img[:, :, 2].astype(float)
+
+    B_ = np.copy(B)
+    G_ = np.copy(G)
+    R_ = np.copy(R)
+
+    K = np.zeros_like(B)
+    C = np.zeros_like(B)
+    M = np.zeros_like(B)
+    Y = np.zeros_like(B)
+
+    for i in range(B.shape[0]):
+        for j in range(B.shape[1]):
+            B_[i, j] = B[i, j]/255
+            G_[i, j] = G[i, j]/255
+            R_[i, j] = R[i, j]/255
+            K[i, j] = 1 - max(B_[i, j], G_[i, j], R_[i, j])
+            if (B_[i, j] == 0) and (G_[i, j] == 0) and (R_[i, j] == 0):
+                # black
+                C[i, j] = 0
+                M[i, j] = 0
+                Y[i, j] = 0
+            else:
+                C[i, j] = (1 - R_[i, j] - K[i, j])/float((1 - K[i, j]))
+                M[i, j] = (1 - G_[i, j] - K[i, j])/float((1 - K[i, j]))
+                Y[i, j] = (1 - B_[i, j] - K[i, j])/float((1 - K[i, j]))
+
+    CMYK = (np.dstack((C, M, Y, K)) * 255).astype(np.uint8)
+    return CMYK
+
+
 path = cv.samples.findFile(
     "/home/caiovinicius/repos/pdi/Processamento-Digital-de-Imagem/implementacoes/images/lena_cor.jpg")
-img = cv.imread(path, 1)  # IMREAD_UNCHANGED
+img = cv.imread(path, cv.IMREAD_UNCHANGED)  # IMREAD_UNCHANGED
 if img is None:
     sys.exit("Could not read the image.")
 h, w, channels = img.shape
@@ -14,34 +88,33 @@ if (channels == 1):
     cv.destroyAllWindows()
 elif (channels == 2):
     cv.imshow("Imagem com dois componentes", img)
-    first, second = decompor(img)
-    cv.imshow("first", first)
-    cv.imshow("second", second)
+    decompor(img)
     cv.waitKey()
     cv.destroyAllWindows()
 elif (channels == 3):
     cv.imshow("Imagem com três componentes", img)
-    first, second, third = decompor(img)
-    cv.imshow("first", first)
-    cv.imshow("second", second)
-    cv.imshow("third", third)
-    cv.waitKey()
-    cv.destroyAllWindows()
+    decompor(img)
     # converter
-    cv.imshow("RGB para HSB", cv.cvtColor(img, cv.COLOR_BGR2HSV))
-    cv.imshow("RGB para YUV", cv.cvtColor(img, cv.COLOR_BGR2YUV))
-    # cv.imshow("RGB para CMYK", cv.cvtColor(img, cv.COLOR_BGR2CMYK))
+    hsb = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    yuv = cv.cvtColor(img, cv.COLOR_BGR2YUV)
+    cmyk = cvtBGR2CMYK(img)
+    # print(hsb[0, 0], yuv[0, 0], cmyk[0, 0])
+    # cv.imshow("BGR para HSB", hsb)
+    # decompor(hsb)
+    # cv.waitKey(10000)
+    # cv.destroyAllWindows()
+    # cv.imshow("BGR para YUV", yuv)
+    # decompor(yuv)
+    cv.imshow("BGR para CMYK", cmyk)
+    decompor(cmyk)
+    # cv.waitKey()
+    # cv.destroyAllWindows()
 else:
     cv.imshow("Imagem com quatro componentes", img)
-    first, second, third, fourth = decompor(img)
-    cv.imshow("first", first)
-    cv.imshow("second", second)
-    cv.imshow("third", third)
-    cv.imshow("fourth", fourth)
-    cv.waitKey()
+    decompor(img)
+    cv.waitKey(10000)
     cv.destroyAllWindows()
-    # convertendo
 
-cv.waitKey()
+cv.waitKey(10000)
 cv.destroyAllWindows()
 # Primeiro eu tenho que mostrar a imagem colorida e em seguida, mostrar as componentes dela.
